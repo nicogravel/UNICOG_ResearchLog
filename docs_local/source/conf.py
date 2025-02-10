8d80103724eae875c913de81a0e508183632e6c6
nicogravel <nicolas.gravel@gmail.com>
2025-02-10 17:08:03 +0100

    replace setup.py for pyproject.toml

--- docs_local/source/conf.py
+++ docs_local/source/conf.py
@@ -137,7 +137,7 @@ cd  /home/nicolas/Documents/GitHubProjects/UNICOG_ResearchLog/
 
 # Add, commit and push the changes
 git add .
-git commit -m "adding doi as link to suggested ref"
+git commit -m "replace setup.py for pyproject.toml"
 git push -u origin main
 
 
--- setup.py
+++ setup.py
@@ -1,6 +1,3 @@
 from setuptools import find_packages, setup
 
-setup(
-    name='myCodeIsYourCode',
-    packages=find_packages(),
-)
+setup( name='myCodeIsYourCode', packages=find_packages(), )
